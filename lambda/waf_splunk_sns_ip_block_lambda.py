91386611e119d726f1d1627b5c8883f945b366ec
security-man <zac.laker@crowncommercial.gov.uk>
2024-03-12 11:14:37 +0000

    Updated requirements.txt and python code

---
+++ lambda/waf_splunk_sns_ip_block_lambda.py
@@ -0,0 +1,65 @@
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    subject = event['Records'][0]['Sns']['Subject']
+    message = event['Records'][0]['Sns']['Message']
+    try:
+        modified_message_content = modify_message_content(message=message)
+        ip_list = []
+        for item in modified_message_content:
+            ip_list.append(item+'/32')
+        
+        waf_client_cloudfront = boto3.client('wafv2',region_name='us-east-1')
+        waf_client_regional = boto3.client('wafv2',region_name='eu-west-2')
+
+        waf_ip_sets_cloudfront = waf_client_cloudfront.list_ip_sets(
+            Scope = 'CLOUDFRONT'
+        )
+        waf_ip_sets_regional = waf_client_regional.list_ip_sets(
+            Scope = 'REGIONAL'
+        )
+        for item in waf_ip_sets_cloudfront['IPSets']:
+            if item['Name']=="waf-splunk-ip-set":
+                waf_ip_set_id_cloudfront = item['Id']
+        for item in waf_ip_sets_regional['IPSets']:
+            if item['Name']=="waf-splunk-ip-set":
+                waf_ip_set_id_regional = item['Id']
+        
+        waf_result_cloudfront = waf_client_cloudfront.get_ip_set(
+            Name='waf-splunk-ip-set',
+            Scope='CLOUDFRONT',
+            Id=waf_ip_set_id_cloudfront
+        )
+        waf_result_regional = waf_client_regional.get_ip_set(
+            Name='waf-splunk-ip-set',
+            Scope='REGIONAL',
+            Id=waf_ip_set_id_regional
+        )
+        token_cloudfront = waf_result_cloudfront['LockToken']
+        token_regional = waf_result_regional['LockToken']
+        response_cloudfront = waf_client_cloudfront.update_ip_set(
+            Name='waf-splunk-ip-set',
+            Scope='CLOUDFRONT',
+            Id=waf_ip_set_id_cloudfront,
+            Description='List of IPs to BLOCK from Splunk due to multiple malicious requests detected',
+            Addresses=ip_list,
+            LockToken=token_cloudfront
+        )
+        response_regional = waf_client_regional.update_ip_set(
+            Name='waf-splunk-ip-set',
+            Scope='REGIONAL',
+            Id=waf_ip_set_id_regional,
+            Description='List of IPs to BLOCK from Splunk due to multiple malicious requests detected',
+            Addresses=ip_list,
+            LockToken=token_regional
+        )
+    except ClientError as e:
+        logging.error(e)
+
+def modify_message_content(message):
+    modified_message_content = list(message.split(","))[:1]
+    print(modified_message_content)
+    return modified_message_content
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 5.32"
     }
   }
   backend "s3" {
@@ -17,7 +17,7 @@ terraform {
 
 # Terraform provider config
 provider "aws" {
-  alias = "default"
+  alias   = "default"
   region  = var.region
   profile = "564059153434-Admin"
   default_tags {
@@ -31,7 +31,7 @@ provider "aws" {
 provider "aws" {
   region  = "us-east-1"
   profile = "564059153434-Admin"
-  alias = "us-east-1"
+  alias   = "us-east-1"
   default_tags {
     tags = {
       ENVIRONMENT = var.ENVIRONMENT
@@ -40,10 +40,12 @@ provider "aws" {
   }
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  provider = aws.default
+}
 
 resource "aws_sns_topic" "WAFSplunkTopic" {
-  provider = aws.default
+  provider          = aws.default
   name              = "waf_splunk_topic"
   kms_master_key_id = var.kms_key_id
   delivery_policy = jsonencode({
@@ -95,14 +97,14 @@ resource "aws_sns_topic" "WAFSplunkTopic" {
 }
 
 resource "aws_sns_topic_subscription" "WAFSplunkTopicTarget" {
-  provider = aws.default
+  provider  = aws.default
   topic_arn = aws_sns_topic.WAFSplunkTopic.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.WAFSplunkLambda.arn
+  endpoint  = module.lambda_function.lambda_function_arn
 }
 
 resource "aws_wafv2_ip_set" "WAFSplunkIPSetCloudFront" {
-  provider = aws.us-east-1
+  provider           = aws.us-east-1
   name               = "waf-splunk-ip-set-cloudfront"
   description        = "A set of IPs updated by Splunk for blocking via WAF for CloudFront"
   scope              = "CLOUDFRONT"
@@ -111,7 +113,7 @@ resource "aws_wafv2_ip_set" "WAFSplunkIPSetCloudFront" {
 }
 
 resource "aws_wafv2_ip_set" "WAFSplunkIPSetRegional" {
-  provider = aws.default
+  provider           = aws.default
   name               = "waf-splunk-ip-set-regional"
   description        = "A set of IPs updated by Splunk for blocking via WAF for eu-west-2"
   scope              = "REGIONAL"
@@ -119,85 +121,130 @@ resource "aws_wafv2_ip_set" "WAFSplunkIPSetRegional" {
   addresses          = []
 }
 
-data "aws_iam_policy_document" "LambdaAssumeRole" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
+module "lambda_function" {
+  providers = {
+    aws = aws.default
   }
+  source = "terraform-aws-modules/lambda/aws"
+  function_name = "waf_splunk_sns_ip_block_lambda"
+  description   = "My awesome lambda function"
+  handler       = "waf_splunk_sns_ip_block_lambda.lambda_handler"
+  runtime       = "python3.11"
+  source_path   = "${path.root}/../lambda"
+  attach_policy_json = true
+  policy_json   = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "wafv2:GetIPSet",
+            "wafv2:ListIPSets",
+            "wafv2:CreateIPSet",
+            "wafv2:UpdateIPSet",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
 }
 
+# data "aws_iam_policy_document" "LambdaAssumeRole" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
+#     actions = [
+#       "sts:AssumeRole"
+#     ]
+#   }
+# }
+
+# resource "aws_iam_role_policy" "LambdaExecuteActions" {
+#   provider = aws.default
+#   name     = "PermissionsPolicyforWAFSplunkLambda"
+#   role     = aws_iam_role.WAFSplunkLambdaIAMRole.id
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "wafv2:GetIPSet",
+#           "wafv2:ListIPSets",
+#           "wafv2:CreateIPSet",
+#           "wafv2:UpdateIPSet",
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
 resource "aws_lambda_permission" "with_sns" {
+  provider      = aws.default
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.WAFSplunkLambda.function_name
+  function_name = "${module.lambda_function.lambda_function_name}"
   principal     = "sns.amazonaws.com"
   source_arn    = aws_sns_topic.WAFSplunkTopic.arn
 }
 
-resource "aws_iam_role" "WAFSplunkLambdaIAMRole" {
-  provider = aws.default
-  name               = "waf_splunk_iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.LambdaAssumeRole.json
-}
+# resource "aws_iam_role" "WAFSplunkLambdaIAMRole" {
+#   provider           = aws.default
+#   name               = "waf_splunk_iam_for_lambda"
+#   assume_role_policy = data.aws_iam_policy_document.LambdaAssumeRole.json
+# }
 
 resource "aws_sns_topic_subscription" "WAFSplunkLambdaSNSSubscription" {
+  provider  = aws.default
   topic_arn = aws_sns_topic.WAFSplunkTopic.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.WAFSplunkLambda.arn
+  endpoint  = "${module.lambda_function.lambda_function_arn}"
 }
 
-data "archive_file" "WAFSplunkLambdaCode" {
-  type        = "zip"
-  source_dir  = "${path.root}/../lambda"
-  output_path = "waf_splunk_lambda_function_payload.zip"
-}
+# data "archive_file" "WAFSplunkLambdaCode" {
+#   type        = "zip"
+#   source_dir  = "${path.root}/../lambda"
+#   output_path = "waf_splunk_lambda_function_payload.zip"
+# }
 
-resource "aws_lambda_function" "WAFSplunkLambda" {
-  provider = aws.default
-  depends_on = [
-    aws_wafv2_ip_set.WAFSplunkIPSetCloudFront,
-    aws_wafv2_ip_set.WAFSplunkIPSetRegional
-  ]
-  filename                       = data.archive_file.WAFSplunkLambdaCode.output_path
-  function_name                  = "waf-splunk-ip-set"
-  description                    = "A lambda processor to read SNS messages sent via Splunk containing malicious IPs which then get added to a WAF IP set"
-  role                           = aws_iam_role.WAFSplunkLambdaIAMRole.arn
-  handler                        = "index.handler"
-  source_code_hash               = data.archive_file.WAFSplunkLambdaCode.output_base64sha256
-  runtime                        = "python3.9"
-  timeout                        = 180
-  memory_size                    = 128
-  reserved_concurrent_executions = -1
-  ephemeral_storage {
-    size = 512
-  }
-  tracing_config {
-    mode = "PassThrough"
-  }
-  architectures = [
-    "x86_64"
-  ]
-}
+# resource "aws_lambda_function" "WAFSplunkLambda" {
+#   provider = aws.default
+#   depends_on = [
+#     aws_wafv2_ip_set.WAFSplunkIPSetCloudFront,
+#     aws_wafv2_ip_set.WAFSplunkIPSetRegional
+#   ]
+#   filename                       = data.archive_file.WAFSplunkLambdaCode.output_path
+#   function_name                  = "waf-splunk-ip-set"
+#   description                    = "A lambda processor to read SNS messages sent via Splunk containing malicious IPs which then get added to a WAF IP set"
+#   role                           = aws_iam_role.WAFSplunkLambdaIAMRole.arn
+#   handler                        = "waf_splunk_lambda_function_payload.lambda_handler"
+#   source_code_hash               = data.archive_file.WAFSplunkLambdaCode.output_base64sha256
+#   runtime                        = "python3.9"
+#   timeout                        = 180
+#   memory_size                    = 128
+#   reserved_concurrent_executions = -1
+#   ephemeral_storage {
+#     size = 512
+#   }
+#   tracing_config {
+#     mode = "PassThrough"
+#   }
+#   architectures = [
+#     "x86_64"
+#   ]
+# }
 
-resource "aws_lambda_permission" "WAFSplunkLambdaLogging" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.WAFSplunkLambda.function_name
-  principal     = "logs.eu-west-2.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_log_group.WAFSplunkLambda.arn}:*"
-}
-
-resource "aws_cloudwatch_log_group" "WAFSplunkLambda" {
-  name = "/WAFSplunkLambda"
-}
-
-resource "aws_cloudwatch_log_subscription_filter" "WAFSplunkLambdaCWLogsSubscription" {
-  depends_on      = [aws_lambda_permission.WAFSplunkLambdaLogging]
-  destination_arn = aws_lambda_function.WAFSplunkLambda.arn
-  filter_pattern  = ""
-  log_group_name  = aws_cloudwatch_log_group.WAFSplunkLambda.name
-  name            = "logging_default"
-}
+# resource "aws_cloudwatch_log_group" "WAFSplunkLambdaLogGroup" {
+#   provider = aws.default
+#   name     = "/aws/lambda/${module.lambda_function.lambda_function_name}"
+# }
